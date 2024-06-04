f15ee039e0176a7bead87c32ccf71aa0f7f1e6b5
saganon <shanon.saga@gmail.com>
2024-06-04 22:22:59 +0700

    mars rover playground

---
+++ mars_rover/mars_rover.js
@@ -0,0 +1,62 @@
+const direction = ["R", "L"];
+
+function marsRover(startDirection, dimenstions, movement) {
+  const moves = movement.split("");
+  let gridX = 0;
+  let gridY = 0;
+  let compass = "N";
+  const dimX = dimenstions[0];
+  const dimY = dimenstions[1];
+  let compassHistory = [];
+
+  for (let index = 0; index < moves.length; index++) {
+    const element = moves[index];
+
+    if (
+      (compassHistory[compassHistory.length - 1] == "W" && element == "R") ||
+      (compassHistory[compassHistory.length - 1] == "E" && element == "L")
+    ) {
+      compass = "S";
+    } else if (
+      (compassHistory[compassHistory.length - 1] == "S" && element == "R") ||
+      (compassHistory[compassHistory.length - 1] == "N" && element == "L")
+    ) {
+      compass = "E";
+    } else if (
+      (compassHistory[compassHistory.length - 1] == "E" && element == "R") ||
+      (compassHistory[compassHistory.length - 1] == "W" && element == "L")
+    ) {
+      compass = "N";
+    } else if (
+      (compassHistory[compassHistory.length - 1] == "N" && element == "R") ||
+      (compassHistory[compassHistory.length - 1] == "S" && element == "L")
+    ) {
+      compass = "W";
+    }
+
+    if (compass == "N" && element == "F") {
+      gridY += 1;
+    } else if (compass == "S" && element == "F") {
+      gridY -= 1;
+    } else if (compass == "W" && element == "F") {
+      gridX += 1;
+    } else if (compass == "E" && element == "F") {
+      gridX -= 1;
+    }
+
+    compassHistory.push(compass);
+  }
+  console.log("compassHistory", compassHistory);
+
+  if (gridX == dimX || gridX == -1 || gridY == dimY || gridY == -1) {
+    return "RIP";
+  }
+
+  return `${gridX}:${gridY}:${compass}`;
+}
+
+// return marsRover([3, 3], "FFR");
+
+module.exports = marsRover;
+
+// ["F", "F"]
---
+++ mars_rover/mars_rover.test.js
@@ -0,0 +1,37 @@
+const marsRover = require("./mars_rover");
+
+test("Mars should be in west 1", () => {
+  expect(marsRover([0, 0], [3, 3], "FFR")).toBe("0:2:W");
+});
+
+test("Mars should be in west 2", () => {
+  expect(marsRover([0, 0], [3, 3], "FFLLL")).toBe("0:2:W");
+});
+
+test("Mars should be in north 1", () => {
+  expect(marsRover([0, 0], [3, 3], "FFRFL")).toBe("1:2:N");
+});
+
+test("Mars should be in east", () => {
+  expect(marsRover([0, 0], [3, 3], "FFRRR")).toBe("0:2:E");
+});
+
+test("Mars should be in south 1", () => {
+  expect(marsRover([0, 0], [3, 3], "FFRFR")).toBe("1:2:S");
+});
+
+test("Mars should be in south 2", () => {
+  expect(marsRover([0, 0], [3, 3], "FFRFRF")).toBe("1:1:S");
+});
+
+test("Mars should be in north 2", () => {
+  expect(marsRover([0, 0], [3, 3], "FF")).toBe("0:2:N");
+});
+
+test("Mars should be in north 3", () => {
+  expect(marsRover([0, 0], [3, 3], "FFRRRR")).toBe("0:2:N");
+});
+
+// test("RIP Mars", () => {
+//   expect(marsRover([0, 0], [3, 3], "FFRLL")).toBe("RIP");
+// });

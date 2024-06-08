c5466c3750532c13695eb3c47cba38dbb46e8b96
Shanon Saga <shanonathooya@gmail.com>
2024-06-09 02:22:07 +0700

    call another class

--- mars_rover_kotlin/src/main/kotlin/Main.kt
+++ mars_rover_kotlin/src/main/kotlin/Main.kt
@@ -1,11 +1,5 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main(): String {
-    val name = "Shanon"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-    return "Hello, Shanon!"
+fun main() {
+    MarsRover().execution()
 }

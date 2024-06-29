5c79a8e5f848f244f1996607680f3476ee884428
Shanon Saga <shanonathooya@gmail.com>
2024-06-29 17:43:59 +0700

    add more test for mars rover

--- mars_rover_kotlin/src/main/kotlin/MarsRover.kt
+++ mars_rover_kotlin/src/main/kotlin/MarsRover.kt
@@ -1,8 +1,42 @@
 package org.example
 
 class MarsRover {
-    fun execution(): String {
-        val name = "Nanami"
-        return "Hello, $name!"
+    fun execution(movement: String = "FFF"): String {
+        val moves = movement.split("")
+        var gridX = 0
+        var gridY = 0
+        var compass = "N"
+        val compassHistory = mutableListOf<String>()
+
+        for (element in moves) {
+            if (compassHistory.size > 0) {
+
+                if ((compassHistory[compassHistory.size - 1] == "W" && element == "R") || (compassHistory[compassHistory.size - 1] == "E" && element == "L")) {
+                    compass = "S"
+                } else if ((compassHistory[compassHistory.size - 1] == "S" && element == "R") || (compassHistory[compassHistory.size - 1] == "N" && element == "L")) {
+                    compass = "E"
+                } else if ((compassHistory[compassHistory.size - 1] == "E" && element == "R") || (compassHistory[compassHistory.size - 1] == "W" && element == "L")) {
+                    compass = "N"
+                } else if ((compassHistory[compassHistory.size - 1] == "N" && element == "R") || (compassHistory[compassHistory.size - 1] == "S" && element == "L")) {
+                    compass = "W"
+                }
+            }
+
+            if (compass == "N" && element == "F") {
+                gridY += 1
+            } else if (compass == "S" && element == "F") {
+                gridY -= 1
+            } else if (compass == "W" && element == "F") {
+                gridX += 1
+            } else if (compass == "E" && element == "F") {
+                gridX -= 1
+            }
+
+            compassHistory.add(compass)
+        }
+
+        println("compassHistory: $compassHistory")
+
+        return "$gridX:$gridY:$compass"
     }
 }
--- mars_rover_kotlin/src/test/kotlin/MarsRoverTest.kt
+++ mars_rover_kotlin/src/test/kotlin/MarsRoverTest.kt
@@ -3,9 +3,13 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class MarsRoverTest {
+    private fun marsRover(movement: String) = MarsRover().execution(movement);
+
     @Test
-    fun testName() {
-        val marsRover = MarsRover().execution()
-        assertEquals("Hello, Nanami!", marsRover)
+    fun marsTest() {
+        assertEquals("0:2:W", marsRover("FFR"))
+        assertEquals("1:2:N", marsRover("FFRFL"))
+        assertEquals("0:2:E", marsRover("FFRRR"))
+        assertEquals("1:2:S", marsRover("FFRFR"))
     }
 }
